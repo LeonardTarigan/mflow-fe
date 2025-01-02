0114e042700960f427419188533c20e7e0b6562a
LeonardTarigan <leontrg32@gmail.com>
2025-01-03 00:54:21 +0700

    refactor: centralize fetch logic in useFetch hook

--- src/repository/api.ts
+++ src/repository/api.ts
@@ -1,1 +1,35 @@
 export const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+
+export const AUTH_API_URL = `${BASE_URL}/auth`;
+
+export async function useFetch<T>(
+  url: string,
+  options: RequestInit = {},
+): Promise<T> {
+  const defaultHeaders = {
+    "Content-Type": "application/json",
+  };
+
+  const mergedOptions: RequestInit = {
+    ...options,
+    headers: {
+      ...defaultHeaders,
+      ...options.headers,
+    },
+  };
+
+  const res = await fetch(url, mergedOptions);
+
+  if (!res.ok) {
+    let errorMessage = "An unknown error occurred";
+    try {
+      const errorResponse = await res.json();
+      errorMessage = errorResponse.error || res.statusText;
+    } catch (err) {
+      console.error("Failed to parse error response:", err);
+    }
+    throw new Error(errorMessage);
+  }
+
+  return res.json();
+}
--- src/repository/auth.repository.ts
+++ src/repository/auth.repository.ts
@@ -2,38 +2,23 @@
 
 import type { ILoginPayload, ILoginResponse } from "@/model/auth.model";
 import type { IResponse } from "@/model/general-types";
-import { BASE_URL } from "./api";
 import { cookies } from "next/headers";
+import { AUTH_API_URL, useFetch } from "./api";
 
 export async function login(
   payload: ILoginPayload,
 ): Promise<IResponse<ILoginResponse>> {
-  const res = await fetch(`${BASE_URL}/auth/login`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(payload),
-  });
+  try {
+    const userData = await useFetch<ILoginResponse>(`${AUTH_API_URL}/login`, {
+      method: "POST",
+      body: JSON.stringify(payload),
+    });
 
-  if (!res.ok) {
-    let errorMessage = "An unknown error occurred";
-    try {
-      const errorResponse = await res.json();
-      errorMessage = errorResponse.error || res.statusText;
-    } catch (err) {
-      console.error("Failed to parse error response:", err);
-    }
+    cookies().set("user", JSON.stringify(userData.user));
+    cookies().set("token", JSON.stringify(userData.token));
 
-    return { error: errorMessage };
+    return { data: userData };
+  } catch (error) {
+    return { error: (error as Error).message };
   }
-
-  const userData: ILoginResponse = await res.json();
-
-  cookies().set("user", JSON.stringify(userData.user));
-  cookies().set("token", JSON.stringify(userData.token));
-
-  return {
-    data: userData,
-  };
 }
